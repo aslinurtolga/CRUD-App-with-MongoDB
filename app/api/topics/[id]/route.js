ab9c6665eac3d9036d7b2c76c9f87e03716b9222
Aslınur Tolga <aslinurtolga@metaworldbilisim.com>
2023-07-10 12:15:18 +0300

    feat: PUT request completed

---
+++ app/api/topics/[id]/route.js
@@ -0,0 +1,9 @@
+import connectMongoDB from "@/libs/mongodb";
+
+export async function PUT(request, { params }) {
+  const { id } = params;
+  const { newTitle: title, newDescription: description } = await request.json();
+  await connectMongoDB();
+  await Topic.findByIdAndUpdate(id, { title, description });
+  return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+}
