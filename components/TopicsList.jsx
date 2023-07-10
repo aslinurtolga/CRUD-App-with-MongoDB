92b7c6e71527d8c4a50cc975c80bc8af969e50f9
Aslınur Tolga <aslinurtolga@metaworldbilisim.com>
2023-07-10 14:27:03 +0300

    feat: try and catch setup

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,7 +2,23 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList() {
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fecth topics");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
   return (
     <>
       <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
