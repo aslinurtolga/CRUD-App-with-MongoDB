3179d917f909639ff48c7b5b5369448e1daca498
Aslınur Tolga <aslinurtolga@metaworldbilisim.com>
2023-07-10 12:42:08 +0300

    feat: GET req. completed

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -1,4 +1,6 @@
 import connectMongoDB from "@/libs/mongodb";
+import Topic from "@/models/topic";
+import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
   const { id } = params;
@@ -6,4 +8,11 @@ export async function PUT(request, { params }) {
   await connectMongoDB();
   await Topic.findByIdAndUpdate(id, { title, description });
   return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+}
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const topic = await Topic.findOne({ _id: id });
+  return NextResponse.json({ topic }, { status: 200 });
 }
