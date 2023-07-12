f6b837afd4ebfebbd9ffb439eac64c5436ad8205
AslinurTolga <aslinurtolga@gmail.com>
2023-07-12 13:47:13 +0300

    fix: page.jsx changed

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,23 +1,25 @@
 import EditTopicForm from "@/components/EditTopicForm";
-import { errorToJSON } from "next/dist/server/render";
 
 const getTopicById = async (id) => {
   try {
-    const res = await fetch(`http://loclhost:3000/api/topics/${id}`, {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
       cache: "no-store",
     });
 
     if (!res.ok) {
       throw new Error("Failed to fetch topic");
     }
+
     return res.json();
   } catch (error) {
     console.log(error);
   }
 };
+
 export default async function EditTopic({ params }) {
   const { id } = params;
   const { topic } = await getTopicById(id);
   const { title, description } = topic;
+
   return <EditTopicForm id={id} title={title} description={description} />;
 }
